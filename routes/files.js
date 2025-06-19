c67ed2271eb289e11d5ba19a6b9fffe30e8f4f79
Romain Jeanniot <jeanniot.webdev@icloud.com>
2025-06-19 14:31:44 +0200

    Privatisation des routes, enregistrement fichiers

---
+++ routes/files.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const router = express.Router();
+
+const service = require('../services/users');
+
+const multer = require('../middlewares/files-storage');
+
+const private = require('../middlewares/private');
+
+
+router.get('/', private.checkJWT, service.getAllFiles);
+
+router.post('/', private.checkJWT, multer, service.createOneFile);
+
+router.get('/:id', private.checkJWT, service.getOneFile);
+
+router.put('/:id', private.checkJWT, multer, service.modifyOneFile);
+
+router.delete('/delete', private.checkJWT, service.deleteOneFile);
+
+module.exports = router;
